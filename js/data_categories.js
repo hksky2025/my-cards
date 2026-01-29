cdc8566d015370e09d595aee7930a41ea4535c1a
hksky2025 <travelggmap@gmail.com>
2026-01-29 22:49:37 +0800

    Add redHotCategories object to data_categories.js

---
+++ js/data_categories.js
@@ -0,0 +1,7 @@
+const redHotCategories = {
+    "dining": ["dining"],
+    "world": ["overseas", "overseas_jkt", "overseas_tw", "overseas_cn", "overseas_other"],
+    "home": ["grocery", "telecom"],
+    "enjoyment": ["transport", "entertainment", "travel"],
+    "style": ["apparel", "health_beauty"]
+};
