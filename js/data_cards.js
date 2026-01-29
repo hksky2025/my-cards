6c78c6632ce8d05d1ae8dcc75e1bcafb6e8c3922
hksky2025 <travelggmap@gmail.com>
2026-01-29 22:49:00 +0800

    Add cardsDB with various credit card details

---
+++ js/data_cards.js
@@ -0,0 +1,11 @@
+const cardsDB = [
+    { id: "hsbc_everymile", name: "✈️ HSBC EveryMile", currency: "HSBC_RC_EM", type: "master", fcf: 0.0195, modules: ["em_base", "em_designated", "em_overseas_bonus", "travel_guru_v2"] },
+    { id: "hsbc_vs", name: "💎 HSBC Visa Signature", currency: "HSBC_RC", type: "visa", fcf: 0.0195, modules: ["vs_base", "vs_red_hot_bonus", "red_hot_variable", "travel_guru_v2"] },
+    { id: "hsbc_red", name: "🍎 HSBC Red Card", currency: "HSBC_RC", type: "master", fcf: 0.0195, modules: ["red_base", "red_online", "red_designated_bonus", "travel_guru_v2"] },
+    { id: "hangseng_mmpower", name: "⚡ Hang Seng MMPower", currency: "Fun_Dollars", type: "master", fcf: 0.0195, modules: ["hangseng_base", "mmpower_overseas_bonus", "mmpower_online_bonus"] },
+    { id: "hangseng_travel_plus", name: "🌍 Hang Seng Travel+", currency: "Fun_Dollars", type: "visa", fcf: 0.0195, modules: ["hangseng_base", "travel_plus_tier1_bonus", "travel_plus_dining_bonus"] },
+    { id: "boc_cheers_vi", name: "🥂 中銀 Cheers VI", currency: "BOC_Points", type: "visa", fcf: 0.0195, modules: ["boc_cheers_base", "boc_cheers_dining", "boc_cheers_travel", "boc_amazing_weekday", "boc_amazing_holiday"] },
+    { id: "sc_smart", name: "🛍️ SC Smart Card", currency: "CASH_Direct", type: "visa", fcf: 0, modules: ["sc_smart_base", "sc_smart_designated"] },
+    { id: "aeon_wakuwaku", name: "🎎 AEON WAKUWAKU", currency: "CASH_Direct", type: "master", fcf: 0.0195, modules: ["aeon_waku_base", "aeon_waku_online"] },
+    { id: "earnmore", name: "💪 EarnMORE 銀聯", currency: "CASH_Direct", type: "unionpay", fcf: 0, modules: ["earnmore_base"] }
+];
