032acab8bbd0d9bae21f9a5f4adb81219f0c01cb
hksky2025 <travelggmap@gmail.com>
2026-01-29 22:50:56 +0800

    Create data_conversions.js

---
+++ js/data_conversions.js
@@ -0,0 +1,8 @@
+const conversionDB = [
+    { src: "HSBC_RC_EM", miles_rate: 20, cash_rate: 1 },
+    { src: "HSBC_RC", miles_rate: 10, cash_rate: 1 },
+    { src: "AM_Direct", miles_rate: 1, cash_rate: 0 },
+    { src: "CASH_Direct", miles_rate: 0, cash_rate: 1 },
+    { src: "Fun_Dollars", miles_rate: 0, cash_rate: 1 },
+    { src: "BOC_Points", miles_rate: 1/15, cash_rate: 0.004 }
+];
